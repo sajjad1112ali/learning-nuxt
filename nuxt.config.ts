196cc6f915b43977064c24d7e90bf9466da11f9b
sajjad ali <sajjadramzan1211@gmail.com>
2023-11-22 23:54:10 +0500

    added tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
   app: {
     head: {
       title: 'My Test Project',
-      link: [
-        {
-          rel: 'stylesheet',
-          href: 'https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css'
-        }
-      ]
+      link: []
     }
   }
 })
