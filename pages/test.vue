f1aa6ec7dcf7f0fb9324de85a94989d7b0acc0c0
Sajjad Ali <sajjadramzan1211@gmail.com>
2023-12-28 16:27:51 +0500

    Dragable images

--- pages/test.vue
+++ pages/test.vue
@@ -1,154 +1,28 @@
 <template>
-  <div>
-    <div class="border p-4">
-      <div class="border p-4 ml-4">
-        Parent 1
-        <div class="border p-4 ml-4">
-          Child 1.1
-        </div>
-      </div>
-    </div>
-  </div>
-  <br>
-  <br>
-  <br>
-  <br>
-
-
-<ul class="tree">
-  <li>
-    <input type="checkbox" checked="checked" id="c1" />
-    <label class="tree_label" for="c1">Level 0</label>
-    <ul>
-      <li>
-        <input type="checkbox" checked="checked" id="c2" />
-        <label for="c2" class="tree_label">Level 1</label>
-      </li>
-    </ul>
-  </li>
-</ul>
+  <div class="flex flex-wrap">
+  <draggable class="" tag="span" :list="list" item-key="url">
+    <template class="" v-for="element in list" :key="element.url">
+      <img class="border-2 border-gray-500 rounded-md w-80 h-auto" :src="element.url" />
+    </template>
+  </draggable>
+</div>
+  <RawDisplayer :value="list" title="List" />
 </template>
-<script></script>
+<script>
 
-<style scoped>
-#fontSizeWrapper { font-size: 16px; }
-
-#fontSize {
-  width: 100px;
-  font-size: 1em;
-  }
-
-/* ————————————————————–
-  Tree core styles
-*/
-.tree { margin: 1em; }
-
-.tree input {
-  position: absolute;
-  clip: rect(0, 0, 0, 0);
-  }
-
-.tree input ~ ul { display: none; }
-
-.tree input:checked ~ ul { display: block; }
-
-/* ————————————————————–
-  Tree rows
-*/
-.tree li {
-  line-height: 1.2;
-  position: relative;
-  padding: 0 0 1em 1em;
-  }
-
-.tree ul li { padding: 1em 0 0 1em; }
-
-.tree > li:last-child { padding-bottom: 0; }
-
-/* ————————————————————–
-  Tree labels
-*/
-.tree_label {
-  position: relative;
-  display: inline-block;
-  background: #fff;
-  }
-
-label.tree_label { cursor: pointer; }
-
-label.tree_label:hover { color: #666; }
-
-/* ————————————————————–
-  Tree expanded icon
-*/
-label.tree_label:before {
-  background: #000;
-  color: #fff;
-  position: relative;
-  z-index: 1;
-  float: left;
-  margin: 0 1em 0 -2em;
-  width: 1em;
-  height: 1em;
-  border-radius: 1em;
-  content: '+';
-  text-align: center;
-  line-height: .9em;
-  }
-
-:checked ~ label.tree_label:before { content: '–'; }
-
-/* ————————————————————–
-  Tree branches
-*/
-.tree li:before {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: -.5em;
-  display: block;
-  width: 0;
-  border-left: 1px solid #777;
-  content: "";
-  }
-
-.tree_label:after {
-  position: absolute;
-  top: 0;
-  left: -1.5em;
-  display: block;
-  height: 0.5em;
-  width: 1em;
-  border-bottom: 1px solid #777;
-  border-left: 1px solid #777;
-  border-radius: 0 0 0 .3em;
-  content: '';
-  }
-
-label.tree_label:after { border-bottom: 0; }
-
-:checked ~ label.tree_label:after {
-  border-radius: 0 .3em 0 0;
-  border-top: 1px solid #777;
-  border-right: 1px solid #777;
-  border-bottom: 0;
-  border-left: 0;
-  bottom: 0;
-  top: 0.5em;
-  height: auto;
-  }
-
-.tree li:last-child:before {
-  height: 1em;
-  bottom: auto;
-  }
-
-.tree > li:last-child:before { display: none; }
-
-.tree_custom {
-  display: block;
-  background: #eee;
-  padding: 1em;
-  border-radius: 0.3em;
-}
-</style>
+import { VueDraggableNext } from 'vue-draggable-next';
+export default {
+  data() {
+    return {
+      list: [
+        { url: "https://image.shutterstock.com/image-illustration/smart-city-nft-virtual-real-260nw-2154735667.jpg", id: 0 },
+        { url: "https://image.shutterstock.com/image-illustration/smart-city-architecture-digital-connected-260nw-2140538579.jpg", id: 1 },
+        { url: "https://image.shutterstock.com/image-vector/metaverse-future-blue-cityscape-perspective-260nw-2153510921.jpg", id: 2 }
+      ],
+    };
+  },
+  components: {
+    draggable: VueDraggableNext
+  },
+};
+</script>
