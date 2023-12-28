b942ac46a13df6bb809085998200904d65ed11ae
Sajjad Ali <sajjadramzan1211@gmail.com>
2023-12-28 15:38:33 +0500

    Date picker

--- pages/about.vue
+++ pages/about.vue
@@ -1,4 +1,17 @@
 <template>
-  <div>About Page</div>
+  <div class="max-w-xl">
+    <br>
+    <br>
+    <br>
+    <VueDatePicker v-model="date" auto-apply placeholder="Select Date" :enable-time-picker="false"></VueDatePicker>
+  <p>{{ date }}</p>
+  </div>
 </template>
-<script lang="ts" setup></script>
+
+<script setup>
+import { ref } from 'vue';
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
+
+const date = ref();
+</script>
